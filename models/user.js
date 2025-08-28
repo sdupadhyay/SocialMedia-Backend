4b94571c7d68ac92d9a18e529c1982404877913a
sdupadhyay <hexahealth@HexaHealths-MacBook-Air.local>
2025-08-28 10:38:52 +0530

    Added Mongoose models with schema defination

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const userSchema = new mongoose.Schema(
+	{
+		firstName: { type: String, required: true, trim: true },
+		lastName: { type: String, required: true, trim: true },
+		email: { type: String, required: true, unique: true, lowercase: true },
+		password: { type: String, required: true },
+		followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+		following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+		savedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+	},
+	{ timestamps: true }
+);
+userSchema.pre("save", async function (next) {
+	try {
+		if (this.isModified("password")) {
+			this.password = await bcrypt.hash(this.password, 10);
+		}
+		next();
+	} catch (err) {
+		return next(err);
+	}
+});
+userSchema.methods.isPasswordCorrect = async function (userPassword) {
+	try {
+		const match = await bcrypt.compare(userPassword, this.password);
+		return match;
+	} catch (err) {
+		throw err;
+	}
+};
+const User = mongoose.model("User", userSchema);
+module.exports = User;
