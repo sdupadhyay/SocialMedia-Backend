45d4aa995afa03780a422248577228be9a842609
sdupadhyay <hexahealth@HexaHealths-MacBook-Air.local>
2025-11-09 17:58:31 +0530

    Updates: Image size and type validation added

--- middleware/multer.js
+++ middleware/multer.js
@@ -2,5 +2,22 @@ const multer = require("multer");
 
 // Configure multer storage (in-memory storage for simplicity)
 const storage = multer.memoryStorage(); // keep file in memory buffer
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 3 * 1024 * 1024, // 3MB per image
+  },
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype === "image/jpeg" ||
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpg" ||
+      file.mimetype === "image/webp"
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only .jpg, .jpeg, .png, .webp files are allowed!"));
+    }
+  },
+});
 module.exports = upload;
